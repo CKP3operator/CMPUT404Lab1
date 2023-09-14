9fd041fa0e9d45bf4eb58e4f8cae79adb5df68d5
CKP3operator <perezcharles1112@gmail.com>
2023-09-13 19:02:58 -0600

    modified code

--- lab1request.py
+++ lab1request.py
@@ -1,5 +1,5 @@
 import requests
 
 #print(requests.__version__)
-r = requests.get('http://google.com')
+r = requests.get('https://raw.githubusercontent.com/CKP3operator/CMPUT404Lab1/main/lab1request.py')
 print(r.text)
